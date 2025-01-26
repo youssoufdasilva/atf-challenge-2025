1b36e1790e575ed7395500adeb1f4592ef96b048
Youssouf da Silva <youssoufdasilva@gmail.com>
2025-01-26 13:38:18 +0000

    Enhance university logos section with responsive design and additional marquee rows

--- src/components/sections/logos.tsx
+++ src/components/sections/logos.tsx
@@ -32,6 +32,9 @@ const first_universities = [
   "UCC",
   "KNUST",
   "Ghana Communication Technology",
+];
+
+const second_universities = [
   "University Academic City",
   "MEST",
   "Tarkoradi Technical University",
@@ -39,13 +42,16 @@ const first_universities = [
   "Accra Institute of Technology",
 ];
 
-const second_universities = [
+const third_universities = [
   "University of Energy and Natural Resources",
   "Ho Technical University",
   "University of Mines and Technology",
   "University of Technology and Applied Sciences",
   "Valley View University",
   "GIMPA",
+];
+
+const fourth_universities = [
   "Lancaster University Ghana",
   "University of the People",
   "SDD University of Business and Integrated Development Studies",
@@ -94,13 +100,12 @@ export function Logos2() {
           Participating universities
         </h3>
         <div className="relative mt-6">
-          {/* <div className="relative flex h-[500px]= mt-6 w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"> */}
           <Marquee pauseOnHover className="[--duration:120s]">
             {first_universities.map((name, idx) => (
               <BlurIn
                 key={`${idx}-first-uni`}
                 word={name}
-                className="text-lg font-bold text-black dark:text-white border-r-8 px-8 -ml-5 border-black"
+                className="text-lg font-bold text-black dark:text-white border-r-4 md:border-r-8 md:px-8 px-2 md:-ml-5 -ml-4 border-black"
               />
             ))}
           </Marquee>
@@ -109,7 +114,25 @@ export function Logos2() {
               <BlurIn
                 key={`${idx}-second-uni`}
                 word={name}
-                className="text-lg font-bold text-black dark:text-white border-r-8 px-8 -ml-5 border-black"
+                className="text-lg font-bold text-black dark:text-white border-r-4 md:border-r-8 md:px-8 px-2 md:-ml-5 -ml-4 border-black"
+              />
+            ))}
+          </Marquee>
+          <Marquee pauseOnHover className="[--duration:120s]">
+            {third_universities.map((name, idx) => (
+              <BlurIn
+                key={`${idx}-third-uni`}
+                word={name}
+                className="text-lg font-bold text-black dark:text-white border-r-4 md:border-r-8 md:px-8 px-2 md:-ml-5 -ml-4 border-black"
+              />
+            ))}
+          </Marquee>
+          <Marquee reverse pauseOnHover className="[--duration:120s]">
+            {fourth_universities.map((name, idx) => (
+              <BlurIn
+                key={`${idx}-fourth-uni`}
+                word={name}
+                className="text-lg font-bold text-black dark:text-white border-r-4 md:border-r-8 md:px-8 px-2 md:-ml-5 -ml-4 border-black"
               />
             ))}
           </Marquee>
